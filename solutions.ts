6c986b3e67e0dbbb1f0f4102f53fe36d13275502
Foysal <md.shihab066@gmail.com>
2025-11-17 23:31:07 +0600

    Add Problem 1 and 2 solution

---
+++ solutions.ts
@@ -0,0 +1,22 @@
+type TFormatValue = string | number | boolean;
+
+const formatValue = (value: TFormatValue): TFormatValue => {
+  switch (typeof value) {
+    case "string":
+      return value.toUpperCase();
+    case "number":
+      return value * 10;
+    case "boolean":
+      return !value;
+  }
+};
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  return 0;
+};
